72ba95b2ea3ceb21a562b83782f793bb5457ab8b
Ashish Pal <ak987982@gmail.com>
2023-04-29 15:20:50 +0530

    Added UC4 and Modified code for play game continue 20 days

--- src/GamblingSimulatorMain.java
+++ src/GamblingSimulatorMain.java
@@ -3,25 +3,38 @@ import java.util.Random;
 public class GamblingSimulatorMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation");
-        int stake = 100;
+        int startingStake = 100;
         int bet = 1;
-        int gamesPlayed = 0;
-        int gamesWon = 0;
+        int daysPlayed = 0;
+        int totalGamesPlayed = 0;
+        int totalGamesWon = 0;
+        int totalAmountWon = 0;
         Random rand = new Random();
 
-        while (stake > 0 && stake < 200 && stake > 50 && stake < 150)  {
-            gamesPlayed++;
-            boolean win = rand.nextBoolean();
-            if (win) {
-                stake += bet;
-                gamesWon++;
-            } else {
-                stake -= bet;
-            }
-        }
+        while (daysPlayed < 20) {
+            int stake = startingStake;
+            int gamesPlayed = 0;
+            int gamesWon = 0;
 
-        System.out.println("Games played: " + gamesPlayed);
-        System.out.println("Games won: " + gamesWon);
-        System.out.println("Final stake: $" + stake);
+            while (stake > 0 && stake < 200 && stake > 50 && stake < 150) {
+                gamesPlayed++;
+                totalGamesPlayed++;
+                boolean win = rand.nextBoolean();
+                if (win) {
+                    stake += bet;
+                    gamesWon++;
+                    totalGamesWon++;
+                } else {
+                    stake -= bet;
+                }
+            }
+            int amountWon = (gamesWon * bet) - (gamesPlayed - gamesWon) * bet;
+            totalAmountWon += amountWon;
+            System.out.println("Day " + (daysPlayed + 1) + ": Games played = " + gamesPlayed + ", Games won = " + gamesWon + ", Amount won = $" + amountWon);
+            daysPlayed++;
+        }
+        System.out.println("Total games played: " + totalGamesPlayed);
+        System.out.println("Total games won: " + totalGamesWon);
+        System.out.println("Total amount won or lost: $" + totalAmountWon);
     }
 }
